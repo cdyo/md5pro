64d7b2bd0d49899a74fa5258cb5c8a5c3362e006
016564 <chendeyong@bonc.com.cn>
2018-04-09 14:24:55 +0800

    md5生成器

---
+++ src/md.java
@@ -0,0 +1,43 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class md {
+    public static void main(String[] args){
+        if(args.length!=1){
+            System.out.println("Please enter the password you want to convert to MD5.");
+            System.out.println("eg. java -jar md5pro.jar hadoop");
+            System.exit(-1);
+        }
+        String passwd = args[0];
+        System.out.println(passwd+" Generating MD5:"+new MD5().md5(passwd));
+    }
+}
+
+class MD5 {
+    private MessageDigest digest;
+    private char hexDigits[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    public MD5() {
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String md5(String str) {
+        byte[] btInput = str.getBytes();
+        digest.reset();
+        digest.update(btInput);
+        byte[] md = digest.digest();
+        // 把密文转换成十六进制的字符串形式
+        int j = md.length;
+        char strChar[] = new char[j * 2];
+        int k = 0;
+        for (int i = 0; i < j; i++) {
+            byte byte0 = md[i];
+            strChar[k++] = hexDigits[byte0 >>> 4 & 0xf];
+            strChar[k++] = hexDigits[byte0 & 0xf];
+        }
+        return new String(strChar);
+    }
+}
